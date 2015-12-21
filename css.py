d498b6fd30757e5a419c3b204899a786bf783027
Matija Folnovic <mfolnovic@gmail.com>
2015-12-21 22:52:46 +0100

    CSS - drugacije racunanje histograma

--- css.py
+++ css.py
@@ -3,21 +3,38 @@ import numpy as np
 from skimage.exposure import histogram
 from scipy.ndimage import zoom
 
-def css(image, bins=32, pixels_per_cell=(8, 8), visualize=False, feature_vector=True):
+def _hist2(cell, bins):
+    d = cell.shape[2]
+    histograms = []
+    for i in range(d):
+        histograms.append(histogram(cell[:, :, i], bins)[0])
+    return np.hstack(histograms)
+
+def _hist(cell, bins):
+    sy, sx, d = cell.shape
+    step = 1.0 / bins
+
+    result = np.zeros((bins * bins * bins))
+    for y in range(sy):
+        for x in range(sx):
+            bin_1 = min(int(cell[y, x, 0] / step), bins - 1)
+            bin_2 = min(int(cell[y, x, 1] / step), bins - 1)
+            bin_3 = min(int(cell[y, x, 2] / step), bins - 1)
+            result[bin_1 * bins * bins + bin_2 * bins + bin_3] += 1
+
+    return result
+
+def css(image, bins=10, pixels_per_cell=(8, 8), visualize=False, feature_vector=True):
     sy, sx, d = image.shape
     cx, cy = pixels_per_cell
 
     n_cellsx = int(np.floor(sx // cx))
     n_cellsy = int(np.floor(sy // cy))
 
-    cells = np.zeros((n_cellsy, n_cellsx, d * bins))
+    cells = np.zeros((n_cellsy, n_cellsx, bins ** d))
     for y in range(n_cellsy):
         for x in range(n_cellsx):
-            histograms = []
-            for i in range(d):
-                cell = image[y*cy:(y+1)*cy, x*cx:(x+1)*cx, i]
-                histograms.append(histogram(cell, bins)[0])
-            cells[y, x, :] = np.hstack(histograms)
+            cells[y, x, :] = _hist(image[y*cy:(y+1)*cy, x*cx:(x+1)*cx, :], bins)
 
     n_cells = n_cellsy * n_cellsx
     pairs = np.zeros((n_cells, n_cells))
@@ -28,25 +45,32 @@ def css(image, bins=32, pixels_per_cell=(8, 8), visualize=False, feature_vector=
             j_y = int(j / n_cellsx)
             j_x = j % n_cellsx
 
-            pairs[i, j] = np.linalg.norm(cells[i_y, i_x, :] - cells[j_y, j_x, :])
-#            pairs[i, j] = np.minimum(cells[i_y, i_x, :], cells[j_y, j_x, :]).sum()
+            if i == j:
+                pairs[i, j] = 0
+            else:
+#                pairs[i, j] = np.linalg.norm(cells[i_y, i_x, :] - cells[j_y, j_x, :])
+                pairs[i, j] = np.minimum(cells[i_y, i_x, :], cells[j_y, j_x, :]).sum()
 
     normalized = pairs / pairs.max()
     
-    if visualize:
+    if hasattr(visualize, "__len__"):
+        indices = visualize
+    elif visualize:
+        indices = range(n_cells)
+    else:
+        indices = []
+
+    if len(indices) > 0:
         result = []
-        for i in range(n_cells):
-            result_image = np.zeros((n_cellsy * cy, n_cellsx * cx))
-            for k in range(n_cells):
-                k_y = int(k / n_cellsx)
-                k_x = k % n_cellsx
-                for l1 in range(cy):
-                    for l2 in range(cx):
-                        result_image[k_y * cy + l1, k_x * cx + l2] = normalized[i, k]
-#            current = [zoom([[cell]], (cy, cx), order=0) for cell in normalized[i, :]]
-#            print(np.array(current).shape)
-#            result_image = np.array(current).reshape((n_cellsy * cy, n_cellsx * cx))
-#            result_image = normalized[i, :].reshape(n_cellsy, n_cellsx)
+        for i in indices:
+#            result_image = np.zeros((n_cellsy * cy, n_cellsx * cx))
+#            for k in range(n_cells):
+#                k_y = int(k / n_cellsx)
+#                k_x = k % n_cellsx
+#                for l1 in range(cy):
+#                    for l2 in range(cx):
+#                        result_image[k_y * cy + l1, k_x * cx + l2] = normalized[i, k]
+            result_image = normalized[i, :].reshape((n_cellsy, n_cellsx))
             result.append(result_image)
         print(result[0].shape)
         return result
--- test.py
+++ test.py
@@ -6,12 +6,18 @@ import matplotlib.cm as cm
 from skimage import data, color
 
 image = data.imread("crop001061.png")
+
+#plt.imshow(image)
+#plt.show()
+
 image_hsv = color.rgb2hsv(image)
-css_images = css(image_hsv, pixels_per_cell=(16, 16), visualize = True)
+
+visualize = [13 * 34 + 14, 25 * 34 + 17, 39 * 34 + 16, 47 * 34 + 28]
+css_images = css(image_hsv, pixels_per_cell=(8, 8), visualize = visualize)
 
 print(len(css_images))
 
-indices = [245, 416]
-for i in indices:
-    plt.imshow(css_images[i], cmap = cm.Greys)
+for i in range(len(css_images)):
+    print(i)
+    plt.imshow(css_images[i], cmap = cm.Greys_r)
     plt.show()
